d8bbedbdb5ac2dab0b69ccbed5e9f3259cab83f7
Jesse Perry <jperry@bethel.jw.org>
2025-05-12 20:03:27 -0600

    created a welcome page and test

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get root_url
+    assert_response :success
+    assert_select "h2", "Hello World"
+    assert_select "p", /The time is now:/
+  end
+
   # test "the truth" do
   #   assert true
   # end
